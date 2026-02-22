8ce4ef5cc1e3c27bda49f3cd242f9f056afeb235
bhuviyashi92-tech <bhuviyashi92@gmail.com>
2026-02-22 18:59:36 +0530

    Add files via upload
    dummy files are uploading

---
+++ sum.py
@@ -0,0 +1,34 @@
+''' 
+a=20
+b=30
+c=a+b
+print("sum of two integers" ,c)
+
+
+
+a=input("enter a val")
+b=input("enter b val")
+c=a+b
+print("sum of two integers",c)
+
+
+
+a=int(input("enter a val"))
+b=int(input("enter b val"))
+c=a+b
+
+print("sum value is=",c)
+      
+
+a=int(input("enter a val"))
+b=int(input("enter b value"))
+c=a-b
+
+print("sub value is=",c)
+
+'''
+
+a=float(input("enter a val"))
+b=float(input("enter b val"))
+c=a+b
+print ("sum of two val is =",c)
